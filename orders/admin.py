ade10ea04724f9c46b911fe75aae517375011d9c
zhanxdani <103193228+zhanxdani@users.noreply.github.com>
2022-06-27 17:33:08 +0600

    Add files via upload

---
+++ orders/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.contrib import admin
+
+# Register your models here.
+from .models import Order, OrderItem
+from django.db.models.query import QuerySet
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'address',
+        'status',
+        'is_paid',
+        'get_status_display',
+        'created',
+        'updated',
+    ]
+    list_filter = ['is_paid', 'status']
+    search_fields = ['id', 'email', 'address']
+    list_editable = ['is_paid']
+    inlines = [OrderItemInline]
+    actions = ['make_status_paid']
+
+    @admin.action(description="Обновить статус оплаты")
+    def make_status_paid(self, request, queryset: QuerySet):
+        queryset.update(is_paid=True)
---
+++ orders/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, reverse
+
+# Create your views here.
+from django.views.generic import FormView
+from .forms import OrderCheckoutForm
+from .models import OrderItem, Order
+from cart.cart import Cart
+
+
+class CheckoutView(FormView):
+    template_name = "checkout.html"
+    form_class = OrderCheckoutForm
+
+    def form_valid(self, form):
+        instance = form.save(commit=False) # Создается Order
+        cart = Cart(self.request)
+        instance.total_sum = cart.get_total_price()
+        instance.save()
+        for item in cart:
+            OrderItem.objects.create(
+                order=instance,
+                quantity=item['quantity'],
+                price=item['price'],
+                product=item['product']
+            )
+        cart.clear()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("products")
